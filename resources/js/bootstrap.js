2c710a5bf05e7c4f4df97742e99ea9384469f37d
Fernando Lopez <fer68769@gmail.com>
2022-04-06 17:15:31 -0500

    Instalacion fancybox

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -33,3 +33,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // Toastr
 window.toastr = require('toastr');
+// FancyBox
+window.Fancybox = require('@fancyapps/ui');
