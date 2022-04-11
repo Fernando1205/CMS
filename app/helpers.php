fc5892122441cda1c3d1b17467c3170bf9d6b55c
Fernando Lopez <fer68769@gmail.com>
2022-04-11 18:05:25 -0500

    Helper para descodificar json de permisos

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,18 @@
 <?php
 
+function keyValueJson($json,$key) {
+    if($json == null){
+        return null;
+    } else {
+        $json = $json;
+        $json = json_decode($json, true);
+        if(array_key_exists($key, $json)){
+            return $json[$key];
+        } else {
+            return null;
+        }
+    }
+}
 function getModulesArray() {
     return $a = [
         '0' => 'Productos',
