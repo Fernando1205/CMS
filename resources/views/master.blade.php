b8d65625694f943a7d434f3f20a71e22548fd194
Fernando Lopez <fer68769@gmail.com>
2022-04-14 14:01:55 -0500

    Nav de pagina

---
+++ resources/views/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="routeName" content="{{ Route::currentRouteName() }}">
+        <title>CMS - @yield('title', 'Laravel')</title>
+
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+        <script src="https://kit.fontawesome.com/80c9d698bb.js" crossorigin="anonymous"></script>
+        @stack('css')
+
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo" width="50px">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('home') }}"><i class="fa-solid fa-house"></i> Inicio</a>
+                        </li>
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}"><i class="fa-solid fa-circle-user"></i> Mi cuenta</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}"><i class="fa-solid fa-user-plus"></i> Crear cuenta</a>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="wrapper">
+            <div class="col2">
+                @yield('content')
+            </div>
+
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+        @stack('script')
+    </body>
+</html>
